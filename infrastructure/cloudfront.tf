e6a459e227cc2abfc48e5b94475498adfaf91895
vmorganp <vmorganp@protonmail.com>
2022-09-22 02:17:32 -0700

    cloudfront stuff again

--- infrastructure/cloudfront.tf
+++ infrastructure/cloudfront.tf
@@ -10,8 +10,12 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     custom_origin_config {
       http_port              = "80"
       https_port             = "443"
-      origin_protocol_policy = "match-viewer"
+      origin_protocol_policy = "https-only"
       origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+    custom_header {
+      name  = "User-Agent"
+      value = random_string.user_agent.result
     }
   }
 
@@ -34,8 +38,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   default_cache_behavior {
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD", "OPTIONS"]
     target_origin_id       = local.origin_id
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
--- infrastructure/s3_bucket.tf
+++ infrastructure/s3_bucket.tf
@@ -1,7 +1,18 @@
+# random user agent
+resource "random_string" "user_agent" {
+  length  = 21
+  special = false
+}
+
 # bucket to hold all of the website html files
 resource "aws_s3_bucket" "host_bucket" {
   bucket = local.site_name
-  acl    = "public-read"
+  # acl    = "public-read"
+  # policy = data.aws_iam_policy_document.s3_bucket_policy.json
+}
+
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = aws_s3_bucket.host_bucket.bucket
   policy = data.aws_iam_policy_document.s3_bucket_policy.json
 }
 
@@ -35,18 +46,20 @@ resource "aws_s3_bucket_website_configuration" "web_config" {
 # policy to allow the buvket to be accessed by cloudfront
 data "aws_iam_policy_document" "s3_bucket_policy" {
   statement {
-    sid = "1"
     actions = [
       "s3:GetObject",
     ]
     resources = [
-      "arn:aws:s3:::vmorganp.com/*",
+      "${aws_s3_bucket.host_bucket.arn}/*",
     ]
     principals {
-      type = "AWS"
-      identifiers = [
-        aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn,
-      ]
+      type        = "*"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:UserAgent"
+      values   = [random_string.user_agent.result]
     }
   }
 }
